d35f6280a7f402f1420620def54b2db37b53ebb8
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-03-17 11:26:12 +0530

    crud operations for posts are created

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const mongoClient = mongodb.MongoClient;
 const DB_URL = 'mongodb://127.0.0.1:27017';
 const DATA_BASE = 'TechBlogs';
 const USERS_COLLECTION = 'users';
+const BLOGS_COLLECTION = 'blogs';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// authenticate
+const Authenticate = async (req, res, next) => {
+	try {
+		const bearer = await req.headers['authorization'];
+		if (!bearer) {
+			return res.json({ message: 'access failed' });
+		} else {
+			jwt.verify(bearer, 'secret', (err, decode) => {
+				if (decode) {
+					console.log(decode);
+					req.body.auth = decode;
+					next();
+				} else {
+					res.json({ message: 'authentication failed' });
+				}
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		res.json({ message: 'Something went wrong in authentication' });
+	}
+};
 
 app.get('/', (req, res) => {
 	res.send('Welcome to Tech Blogs App');
@@ -26,12 +51,124 @@ app.post('/register', async (req, res) => {
 			const hash = await bcrypt.hash(req.body.password, salt);
 			req.body.password = hash;
 			await db.collection(USERS_COLLECTION).insertOne(req.body);
-			res.status(200).json({ message: 'User Added! ' });
+			res.status(200).json({ message: 'User Added!' });
 		}
 		client.close();
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: 'something went wrong' });
+	}
+});
+
+app.post('/login', async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		let user = await db.collection(USERS_COLLECTION).findOne({ email: req.body.email });
+		if (user) {
+			let match = await bcrypt.compare(req.body.password, user.password);
+			console.log(match);
+			if (match) {
+				const token = jwt.sign({ email: req.body.email }, 'secret', { expiresIn: '1h' });
+				console.log(token);
+				res.send({ message: 'Allow', token });
+			}
+		} else {
+			res.send({ message: 'Not Allow' });
+		}
+		client.close();
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'something went wrong' });
+	}
+});
+
+app.post('/postblog', [Authenticate], async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		await db.collection(BLOGS_COLLECTION).insertOne({
+			heading: req.body.heading,
+			subHeading: req.body.subheading,
+			imgUrl: req.body.url,
+			message: req.body.postdiscription,
+			email: req.body.auth.email,
+		});
+		res.status(200).json({ message: 'Post Added to your profile' });
+		client.close();
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'something went wrong' });
+	}
+});
+
+app.get('/blogs', [Authenticate], async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		let blogs = await db.collection(BLOGS_COLLECTION).find().toArray();
+		console.log(posts);
+		client.close();
+		res.status(200).json(blogs);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Something went wrong' });
+	}
+});
+
+app.get('/blogs/:id', [Authenticate], async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		let blog = await db
+			.collection(BLOGS_COLLECTION)
+			.findOne({ _id: mongodb.ObjectID(req.params.id) });
+		// console.log(posts);
+
+		client.close();
+
+		res.status(200).json(blog);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Something went wrong' });
+	}
+});
+
+app.delete('/blogs/:id', [Authenticate], async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		await db.collection(BLOGS_COLLECTION).deleteOne({ _id: mongodb.ObjectID(req.params.id) });
+		// console.log(posts);
+		client.close();
+		res.status(200).json({ message: 'Blog deleted' });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Something went wrong' });
+	}
+});
+
+app.put('/blogs/:id', [Authenticate], async (req, res) => {
+	try {
+		const client = await mongoClient.connect(DB_URL);
+		const db = client.db(DATA_BASE);
+		await db.collection(BLOGS_COLLECTION).updateOne(
+			{ _id: mongodb.ObjectID(req.params.id) },
+			{
+				$set: {
+					heading: req.body.heading,
+					subHeading: req.body.subheading,
+					imgUrl: req.body.url,
+					message: req.body.postdiscription,
+				},
+			}
+		);
+		// console.log(posts);
+		client.close();
+		res.status(200).json({ message: 'Blog Updated' });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Something went wrong' });
 	}
 });
 
